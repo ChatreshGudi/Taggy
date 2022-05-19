1bcf81e014a8ab984e3afe4a5077ae84d9b3abca
Chatresh <chatreshrsg0202@gmail.com>
2022-05-19 18:02:32 +0530

    Update

--- Lib/Taggy.js
+++ Lib/Taggy.js
@@ -2,69 +2,65 @@ class Tag{
     constructor(TagName){
         ' Used to create a new tag. '
         this.TagName = TagName;
-        this.Attr_Names = [];
-        this.Attr_Values = [];
-        this.children = [];
+        this.element = document.createElement(this.TagName);        
         this.innerText = "";
-        this.style = "";
+        this.styleAttr_Name = [];
+        this.styleAttr_Value = [];
     }
+
     setAttr(Attr_Name, Attr_Value){
         ' Used to set an attribute of the tag. '
-        if (Attr_Name in this.Attr_Names){ // Checking if the attribute exists
-            this.Attr_Values[this.Attr_Names.indexOf(Attr_Name)] = Attr_Value; // Updating the value
-        }
-        else{
-            this.Attr_Names.push(Attr_Name); // Adding the attribute
-            this.Attr_Values.push(Attr_Value); // Adding the value
-        }        
+        this.element.setAttribute(Attr_Name, Attr_Value);        
     }
+
     delAttr(Attr_Name){
         ' Used to delete an attribute from the tag. '
-        var index = this.Attr_Names.indexOf(Attr_Name);
-        if(index > -1){ // Checking if the attribute exists
-            this.Attr_Names.splice(index, 1); // Removing the attribute
-            this.Attr_Values.splice(index, 1); // Removing the value
-        }
+        this.element.removeAttribute(Attr_Name);
     }
+
     setInnerText(innerText){
         ' Used to set the inner text of the tag. '
         this.innerText = innerText; // Updating the innerText
     }
+
     addChild(child){
         ' Used to add a sub tag to the current tag. '
-        this.children.push(child); // Adding the child to the children array
+        this.element.appendChild(child); // Adding the child
     }
-    addStyle(style){
+
+    addStyle(styleName, styleValue){
         ' Used to add a style to the tag. '
-        this.style += style; // Adding the style to the style string
-    }
-    getStyle(){
-        ' Used to get the style of the tag. '
-        return this.style; // Returning the style
-    }
-    resetStyle(){
-        ' Used to reset the style of the tag. '
-        this.style = ""; // Resetting the style
-    }
-    stringify(){
-        ' Used to convert the tag to a string. '
-        
-        // Creating the opening tag
-        var str = "<" + this.TagName;
-        for(var i = 0; i < this.Attr_Names.length; i++){
-            str += " " + this.Attr_Names[i] + "=\"" + this.Attr_Values[i] + "\"";
+        if (this.styleAttr_Name in this.styleAttr_Names){ // Checking if the attribute exists
+            this.styleAttr_Values[this.styleAttr_Names.indexOf(styleName)] = styleValue; // Updating the value
         }
+        else{
+            this.styleAttr_Name.push(styleName); // Adding the style to the style string
+            this.styleAttr_Value.push(styleValue); // Adding the value to the style string
+        }        
+    }
 
-        str += " style=\"" + this.style + "\">"; // Adding the style to the opening tag
-        
-        // Adding the inner text
-        str += this.innerText;
+    removeStyle(styleName){
+        ' Used to remove a style from the tag. '
+        var index = this.styleAttr_Name.indexOf(styleName);
+        if(index > -1){ // Checking if the attribute exists
+            this.styleAttr_Name.splice(index, 1); // Removing the attribute
+            this.styleAttr_Value.splice(index, 1); // Removing the value
+        }
+    }
 
-        // Adding the string of children
-        for(var i = 0; i < this.children.length; i++){
-            str += this.children[i].stringify();
+    createStyleString(){
+        ' Used to create the style string. '
+        var str = "";
+        for(var i = 0; i < this.styleAttr_Name.length; i++){
+            str += this.styleAttr_Name[i] + ":" + this.styleAttr_Value[i] + ";";
         }
         return str;
+    }
+
+    getElement(){
+        ' Used to get the element of the tag. '
+        this.element.style = this.createStyleString();
+        return this.element; // Returning the element
     }
 }
 
@@ -96,20 +92,20 @@ input.setAttr("type", "number");
 input.setAttr("value", "0");
 input.setAttr("min", "0");
 input.setAttr("max", "100");
-input.addStyle("outline: none;");
-let div = new Tag("div");
-// div.setAttr("id", "myDiv");
-div.setInnerText("Enter a number between 0 and 100:");
-div.addChild(new Tag("br"));
-div.addChild(input);
-div.addStyle("border: 1px solid skyblue;");
-div.addStyle("padding: 10px;");
-div.addStyle("border-radius: 5px;");
-div.addStyle("box-shadow: 0px 0px 25px skyblue;");
-document.getElementById("output").innerHTML = div.stringify();
+input.addStyle("outline","none");
+// let div = new Tag("div");
+// div.setInnerText("Enter a number between 0 and 100:");
+// div.addChild(input.getElement());
+// div.addStyle("border","1px solid skyblue");
+// div.addStyle("padding", "10px");
+// div.addStyle("border-radius", "5px");
+// div.addStyle("box-shadow", "0px 0px 25px skyblue");
+let inp = document.createElement("input");
+// document.body.appendChild(input.getElement());
+document.body.appendChild(document.createElement("input"));
 
-let inptbx = new Tag("inputBox");
+// let inptbx = new Tag("inputBox");
 
-let inputBox = new CustomTag(inptbx);
-inputBox.define(div);
+// let inputBox = new CustomTag(inptbx);
+// inputBox.define(div);
 // inputBox.publish();
